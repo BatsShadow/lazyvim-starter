865ea398e82bd7d256c242dc92b34ca4016ebe6a
Scott Webber <batsshadow@gmail.com>
2024-05-15 17:19:49 -0400

    Tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Work around something slow in zsh startup
+-- This allows us to swap between nvim and tmux panes without lag
+vim.opt.shell = "/bin/bash -i"
+
+-- Disable minipairs. Pairs are the worst
+vim.g.minipairs_disable = true
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,22 +1,44 @@
 return {
+  -- {
+  --   "loctvl842/monokai-pro.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent_background = true,
+  --     filter = "spectrum",
+  --   },
+  --   config = function(_, opts)
+  --     local monokai = require("monokai-pro")
+  --     monokai.setup(opts)
+  --     monokai.load()
+  --   end,
+  -- },
   {
-    "loctvl842/monokai-pro.nvim",
+    "catppuccin/nvim",
     lazy = false,
+    name = "catppuccin",
     priority = 1000,
     opts = {
+      flavour = "mocha",
       transparent_background = true,
-      filter = "spectrum",
     },
-    config = function(_, opts)
-      local monokai = require("monokai-pro")
-      monokai.setup(opts)
-      monokai.load()
-    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "monokai-pro",
+      -- colorscheme = "monokai-pro",
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/noice.nvim",
+    config = {
+      cmdline = {
+        view = "cmdline",
+        format = {
+          cmdline = { icon = ":" },
+        },
+      },
     },
   },
 }
