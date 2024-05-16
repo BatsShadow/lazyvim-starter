8e17a53194101593c9737e06951d2fe8c10051bd
Scott Webber <batsshadow@gmail.com>
2024-05-16 10:44:21 -0400

    More customizations:
    - Add Oil
    - Remap - to oil, and shift-_ to split
    - Hide diag virtual text, show blame instead
    - Plugin updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Replace l-dash split with oil, and use l-shift-dash for split
+vim.keymap.set("n", "<leader>-", vim.cmd.Oil)
+vim.keymap.set("n", "<leader>_", ":sb<CR>")
